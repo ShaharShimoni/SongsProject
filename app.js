4a181a352a713d09f35cee8066c919342c1bd4ba
shaharivka <57682267+shaharivka@users.noreply.github.com>
2021-09-12 04:04:13 +0300

    Finish

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 var __dirname='views';
 
+
 var PORT=process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Application started and Listening on port 3000");
